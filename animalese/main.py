bf030cb4a64d375f92d714a6dce22909a6bc8520
DigiDuncan <digiduncan@gmail.com>
2020-04-04 02:11:30 -0400

    Very unstable main.

--- animalese/main.py
+++ animalese/main.py
@@ -1,16 +1,35 @@
-# import pydub
+from pydub import AudioSegment
+from pydub.playback import play
 
-from animalese.lib import constants
 from animalese.lib.constants import ENGLISH
+
+
+def add_character(soundfile, n, newchar, offset):
+    character = newchar.upper()
+    if character in ENGLISH_LETTERS:
+        soundfile = soundfile.overlay(ENGLISH[character], position = n * offset)
+    if character == " ":
+        soundfile = soundfile.overlay(AudioSegment.empty(), position = n * (offset * SPACE_WIDTH))
+
+
+def calclength(text, offset):
+    ends = [len(ENGLISH[c.upper()]) + offset * n for n, c in enumerate(text) if c.upper() in ENGLISH_LETTERS]
+    maxend = max(ends)
+    return maxend
+
+
+def to_animalese(text):
+    return AudioSegment.empty()
 
 
 def main():
     print("Input text:")
     text = input("> ")
 
-    for c in text:
-        letter = c.upper()
-        print(ENGLISH[letter])
+    outsound = to_animalese(text)
+
+    play(outsound)
+    print("Done!")
 
 
 if __name__ == "__main__":
